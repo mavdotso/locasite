8effbb32f99a9ff3b3f8be8d5801b55f7a881789
mav <0xmav.eth@gmail.com>
2025-07-23 20:09:09 +0700

    Fix auth.config.ts to use process.env.CONVEX_SITE_URL

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,10 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { convexEnv } from "./lib/env";
-
 export default {
     providers: [
         {
-            domain: convexEnv.CONVEX_SITE_URL,
+            domain: process.env.CONVEX_SITE_URL,
             applicationID: 'convex',
         },
     ],
